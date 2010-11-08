484d514de63b51838e6225ed375a7a7fa036c2a9
amardaxini <amardaxini@gmail.com>
2010-11-08 15:13:52 +0530

    Added rescue and modified pasco command usage

--- lib/pasco.rb
+++ lib/pasco.rb
@@ -14,9 +14,13 @@ def initialize(url="",modified_time="",access_time="",file_name="",directory="",
  def self.get_history(file_name)
    if !file_name.nil? &&  File.extname(file_name) == ".dat"
      if RUBY_PLATFORM=~ /linux/ || RUBY_PLATFORM =~ /darwin/
-        result =`pasco "#{file_name}"`
-     else #FOR Windows Ihave Not tested yet
-       result=`pasco.exe "#{file_name}"`
+        pasco_path = File.expand_path(File.join("pasco"))
+        command = "#{pasco_path} #{file_name}"
+        result =system(command)
+     else #FOR Windows Not tested yet
+        pasco_path = File.expand_path(File.join("pasco.exe"))
+        command = "#{pasco_path} #{file_name}"
+        result =system(command)
      end  
      @ie_histories = self.process_history(result)
    else
@@ -33,13 +37,15 @@ def initialize(url="",modified_time="",access_time="",file_name="",directory="",
     results.each do |result|
       if !result.match(/^URL/).nil?
         result_of_url = result.split("\t")
-        browsing_url = result_of_url[1].match(/((\w+):\/\/(.+))/)[0]
+        browsing_url = result_of_url[1].match(/((\w+):\/\/(.+))/)[0] rescue nil
         modified_time = result_of_url[2]
         acess_time = result_of_url[3]
         file_name = result_of_url[4]
         directory = result_of_url[5]
         http_header = result_of_url[6]
-        ie_histories << Pasco.new(browsing_url,modified_time,acess_time,file_name,directory,http_header)
+        if !browsing_url.nil?
+         ie_histories << Pasco.new(browsing_url,modified_time,acess_time,file_name,directory,http_header)
+       end 
       end
     end
     ie_histories
